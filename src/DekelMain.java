5f9eb8c0331f5a0f4421b41ebf90c1f14e3bd2c1
dekelDahan <dekeldahan45@gmail.com>
2019-01-12 14:59:20 +0200

    new commit of dekel2

--- src/DekelMain.java
+++ src/DekelMain.java
@@ -1,6 +1,6 @@
 public class DekelMain {
     public static void main(String[] args) {
-        System.out.println("SHaron2");
+        System.out.println("SHaron3");
         System.out.println("Dahan");
     }
 }
